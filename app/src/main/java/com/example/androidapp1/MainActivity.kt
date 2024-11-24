bdc1809a325b143d72104d4cfa04fd8a91031f2b
Jolo Ortiz <lourdzitro@gmail.com>
2024-11-24 15:42:53 +0800

    Adding surname

--- app/src/main/java/com/example/androidapp1/MainActivity.kt
+++ app/src/main/java/com/example/androidapp1/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     AndroidApp1Theme {
-        Greeting("Jolo")
+        Greeting("Jolo Ortiz")
     }
 }
